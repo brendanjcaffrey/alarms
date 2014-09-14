2fe518c94769d125dc186b8f1039c9b7b27c2cf7
Brendan Caffrey <brendan@jcaffrey.com>
2014-09-14 02:41:47 -0500

    cleaning up leap motion interface to use delegate pattern

--- app/leap_motion_talker.rb
+++ app/leap_motion_talker.rb
@@ -1,26 +1,16 @@
 class LeapMotionTalker
-  @start_talking_message = 'LeapMotionTalkerStartTalkingMessage'
-  @stop_talking_message = 'LeapMotionTalkerStopTalkingMessage'
-  @did_gesture_message = 'LeapMotionTalkerDidRecognizeGestureMessage'
-
-  class << self
-    attr_reader :start_talking_message, :stop_talking_message, :did_gesture_message
+  def initialize(delegate)
+    @delegate = WeakRef.new(delegate)
   end
 
-  def initialize
-    center = NSNotificationCenter.defaultCenter
-    center.addObserver(self, selector:'start_talking:', name:LeapMotionTalker.start_talking_message, object:nil)
-    center.addObserver(self, selector:'stop_talking:', name:LeapMotionTalker.stop_talking_message, object:nil)
-  end
-
-  def start_talking(notification)
+  def start_talking
     url = NSURL.URLWithString('ws://127.0.0.1:6437')
     @socket = SRWebSocket.alloc.initWithURLRequest(NSURLRequest.requestWithURL(url))
     @socket.delegate = self
     @socket.open
   end
 
-  def stop_talking(notification)
+  def stop_talking
     @socket.close
   end
 
@@ -38,7 +28,7 @@ class LeapMotionTalker
       puts error.userInfo[NSLocalizedDescriptionKey]
     else
       return if parsed['gestures'].nil? or parsed['gestures'].empty?
-      NSNotificationCenter.defaultCenter.postNotificationName(LeapMotionTalker.did_gesture_message, object:self)
+      @delegate.did_gesture
     end
   end
 
--- spec/leap_motion_talker_spec.rb
+++ spec/leap_motion_talker_spec.rb
@@ -1,8 +1,5 @@
 describe LeapMotionTalker do
   it 'should start listening when it receives the appropriate message, send a message when it receives a gesture and close when it receives the stop message' do
-    @received = false
-    NSNotificationCenter.defaultCenter.addObserver(self, selector:'receiveNotification:', name:LeapMotionTalker.did_gesture_message, object:nil)
-
     socket_stub = Object.new
     socket_stub.mock!(:delegate=) { |obj| obj.should != nil }
     socket_stub.mock!(:open)
@@ -12,18 +9,14 @@ describe LeapMotionTalker do
     initial_stub = stub(:initWithURLRequest) { |url| socket_stub }
     SRWebSocket.mock!(:alloc, return: initial_stub)
 
-    talker = LeapMotionTalker.new
-    NSNotificationCenter.defaultCenter.postNotificationName(LeapMotionTalker.start_talking_message, object:self)
+    delegate = mock(:did_gesture)
+    talker = LeapMotionTalker.new(delegate)
+    talker.start_talking
 
     talker.webSocketDidOpen(socket_stub)
     json = '{"gestures":"hi"}'
     talker.webSocket(socket_stub, didReceiveMessage:json)
 
-    NSNotificationCenter.defaultCenter.postNotificationName(LeapMotionTalker.stop_talking_message, object:self)
-    @received.should == true
-  end
-
-  def receiveNotification(notification)
-    @received = true
+    talker.stop_talking
   end
 end
