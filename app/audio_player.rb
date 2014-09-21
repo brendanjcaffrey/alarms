27de2bd52d08f752628f0d88aa129ed1438da770
Brendan Caffrey <brendan@jcaffrey.com>
2014-09-21 16:32:52 -0500

    adding slowly increasing volume alarm and replacing the volume switch code to work with headphones

--- app/audio_player.rb
+++ app/audio_player.rb
@@ -1,6 +1,8 @@
 class AudioPlayer
   @@default_output = 'Built-in Output'
   @@max_volume = 1.0
+  @@start_volume = 0.05
+  @@increase_by = 0.01
 
   def initialize
     error_ptr = Pointer.new(:object)
@@ -16,18 +18,38 @@ class AudioPlayer
   end
 
   def play
+    @timer = NSTimer.scheduledTimerWithTimeInterval(1.0, target:self, selector:'update_volume', userInfo:nil, repeats:true)
     @old_output = getCurrentOutputDeviceName
     setOutputDeviceByName(@@default_output)
 
     @old_volume = getOutputVolume
-    setOutputVolume(@@max_volume)
+    @current_volume = @@start_volume - @@increase_by
+    update_volume
 
     @player.play
   end
 
+  def update_volume
+    @current_volume = @current_volume + @@increase_by
+    if @current_volume >= @@max_volume
+      @current_volume = 1.0
+      invalidate_timer
+    end
+    setOutputVolume(@current_volume)
+  end
+
   def stop
+    invalidate_timer
+    @player.stop
     setOutputVolume(@old_volume)
     setOutputDeviceByName(@old_output)
-    @player.stop
+  end
+
+  private
+
+  def invalidate_timer
+    return if @timer.nil? || !@timer.isValid
+    @timer.invalidate
+    @timer = nil
   end
 end
--- vendor/switchvolume/volume_switch.c
+++ vendor/switchvolume/volume_switch.c
@@ -1,69 +1,119 @@
-//  Created by Rodrigo Damazio on 24/01/10.
-//  Strongly based on code found on http://www.cocoadev.com/index.pl?SoundVolume
-//  Modified by Brendan Caffrey, originally from https://code.google.com/p/android-notifier/
+//
+//  SoundController.m
+//
+//  Serial Remote for Mac - Control mac actions via serial commands
+//  Copyright (C) 2010  Jeremiah McConnell
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+//  Originally from http://serial-remote-mac.googlecode.com/svn-history/r11/trunk/SoundController.m,
+//  modified here
 
 #include "volume_switch.h"
 
-AudioDeviceID getOutputDeviceId() {
-  AudioDeviceID deviceID = kAudioDeviceUnknown;
-  UInt32 propertySize = sizeof(deviceID);
-  AudioHardwareGetProperty(kAudioHardwarePropertyDefaultOutputDevice, &propertySize, &deviceID);
-  return deviceID;
+AudioDeviceID outputDeviceID = kAudioObjectUnknown;
+
+void getOutputDeviceId() {
+  UInt32 propertySize = 0;
+  OSStatus status = noErr;
+  AudioObjectPropertyAddress propertyAOPA;
+
+  propertyAOPA.mElement = kAudioObjectPropertyElementMaster;
+  propertyAOPA.mScope = kAudioObjectPropertyScopeGlobal;
+  propertyAOPA.mSelector = kAudioHardwarePropertyDefaultOutputDevice;
+  propertySize = sizeof(AudioDeviceID);
+
+  status = AudioHardwareServiceGetPropertyData(kAudioObjectSystemObject, &propertyAOPA, 0, NULL, &propertySize, &outputDeviceID); 
+
+  if(status) {
+    printf("Error setting output device id");
+    // Error
+    return;
+  }
 }
 
 float getOutputVolume() {
+  getOutputDeviceId();
+
   Float32 outputVolume;
   UInt32 propertySize = 0;
   OSStatus status = noErr;
-
   AudioObjectPropertyAddress propertyAOPA;
+
   propertyAOPA.mElement = kAudioObjectPropertyElementMaster;
-  propertyAOPA.mSelector = kAudioHardwareServiceDeviceProperty_VirtualMasterVolume;
   propertyAOPA.mScope = kAudioDevicePropertyScopeOutput;
+  propertyAOPA.mSelector = kAudioHardwareServiceDeviceProperty_VirtualMasterVolume;
+  propertySize = sizeof(Float32);
 
-  AudioDeviceID outputDeviceID = getOutputDeviceId();
-  if (outputDeviceID == kAudioObjectUnknown) return 0.0f;
-  if (!AudioHardwareServiceHasProperty(outputDeviceID, &propertyAOPA)) return 0.0f;
-
-  propertySize = (UInt32) sizeof(Float32);
   status = AudioHardwareServiceGetPropertyData(outputDeviceID, &propertyAOPA, 0, NULL, &propertySize, &outputVolume);
-  if (status) return 0.0f;
 
-  if (outputVolume < 0.0f) return 0.0f;
-  if (outputVolume > 1.0f) return 1.0f;
+  if(status) {
+    printf("getOutputVolume error");
+    return 0.5;
+  }
 
-  return outputVolume;
+  if (outputVolume < 0.0) return 0.0;
+  else if (outputVolume > 1.0) return 1.0;
+  else return outputVolume;
 }
 
 void setOutputVolume(float newVolume) {
   if (newVolume < 0.0f) newVolume = 0.0f;
   if (newVolume > 1.0f) newVolume = 1.0f;
 
+  getOutputDeviceId();
+
   UInt32 propertySize = 0;
   OSStatus status = noErr;
-
   AudioObjectPropertyAddress propertyAOPA;
   propertyAOPA.mElement = kAudioObjectPropertyElementMaster;
   propertyAOPA.mScope = kAudioDevicePropertyScopeOutput;
-  propertyAOPA.mSelector = kAudioHardwareServiceDeviceProperty_VirtualMasterVolume;
 
-  AudioDeviceID outputDeviceID = getOutputDeviceId();
-  if (!AudioHardwareServiceHasProperty(outputDeviceID, &propertyAOPA)) return;
+  if (newVolume < 0.001) {
+    propertyAOPA.mSelector = kAudioDevicePropertyMute;
+    propertySize = sizeof(UInt32);
+    UInt32 mute = 1;
+    status = AudioHardwareServiceSetPropertyData(outputDeviceID, &propertyAOPA, 0, NULL, propertySize, &mute);
 
-  Boolean canSetVolume = false;
-  status = AudioHardwareServiceIsPropertySettable(outputDeviceID, &propertyAOPA, &canSetVolume);
-  if (status || canSetVolume == false) return;
+    if(status) {
+      printf("Unable to mute device");
+      return;
+    }
+  } else {
+    propertyAOPA.mSelector = kAudioHardwareServiceDeviceProperty_VirtualMasterVolume;
+    Float32 volumeToSet = newVolume;
+    propertySize = sizeof(Float32);
 
-  propertySize = (UInt32) sizeof(Float32);
-  status = AudioHardwareServiceSetPropertyData(outputDeviceID, &propertyAOPA, 0, NULL, propertySize, &newVolume);	
+    status = AudioHardwareServiceSetPropertyData(outputDeviceID, &propertyAOPA, 0, NULL, propertySize, &volumeToSet);
 
-  propertyAOPA.mSelector = kAudioDevicePropertyMute;
-  propertySize = (UInt32) sizeof(UInt32);
-  UInt32 mute = 0;
-  if (!AudioHardwareServiceHasProperty(outputDeviceID, &propertyAOPA)) return;
+    if(status) {
+      printf("Unable to set volume.\n");
+      // Error
+      return;
+    }
 
-  Boolean canSetMute = false;
-  status = AudioHardwareServiceIsPropertySettable(outputDeviceID, &propertyAOPA, &canSetMute);
-  if (status || !canSetMute) return;
-  status = AudioHardwareServiceSetPropertyData(outputDeviceID, &propertyAOPA, 0, NULL, propertySize, &mute);
+    // make sure we're not muted
+    propertyAOPA.mSelector = kAudioDevicePropertyMute;
+    propertySize = sizeof(UInt32);
+    UInt32 mute = 0;
+
+    status = AudioHardwareServiceSetPropertyData(outputDeviceID, &propertyAOPA, 0, NULL, propertySize, &mute);
+
+    if(status) {
+      printf("Unable to unmute\n");
+      // Error
+      return;
+    }
+  }
 }
