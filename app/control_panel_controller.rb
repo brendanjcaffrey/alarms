716f69998ca37b0893fa164772b19b4c526dbb21
Brendan Caffrey <brendan@jcaffrey.com>
2014-10-13 15:35:30 -0500

    enter/esc to stop alarm, space to snooze
    plus, the alarm window constantly focuses itself and makes it key, so you can never lose it and key events always go to it

--- app/control_panel_controller.rb
+++ app/control_panel_controller.rb
@@ -10,6 +10,8 @@ class ControlPanelController < NSWindowController
     super.tap do
       @layout = ControlPanelLayout.alloc.init
       self.window = @layout.window
+      self.window.instance_eval { def canBecomeKeyWindow; true; end }
+
       update_time
 
       snooze = @layout.snooze
@@ -20,6 +22,15 @@ class ControlPanelController < NSWindowController
       stop.setTarget(self)
       stop.setAction('stop:')
     end
+  end
+
+  def keyDown(event)
+    stop(event) if event.characters == "\r"
+    snooze(event) if event.characters == " "
+  end
+
+  def cancel(event)
+    stop(event)
   end
 
   def snooze(sender)
@@ -35,6 +46,9 @@ class ControlPanelController < NSWindowController
   def update_time
     @formatter ||= NSDateFormatter.alloc.init.tap { |format| format.setDateFormat 'hh:mm:ss a' }
     @layout.time.setStringValue(@formatter.stringFromDate(NSDate.date))
+
+    self.window.makeKeyAndOrderFront(self)
+    NSApp.activateIgnoringOtherApps(true)
   end
 
   def close_window
