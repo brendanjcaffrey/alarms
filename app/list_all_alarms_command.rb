06ae9b6a700abd9fa22f79a4d4ed284cc8f356e9
Brendan Caffrey <brendan@jcaffrey.com>
2014-10-18 08:41:48 -0500

    return a message instead of nothing in list all alarms applescript command

--- app/list_all_alarms_command.rb
+++ app/list_all_alarms_command.rb
@@ -2,6 +2,7 @@ class ListAllAlarmsCommand < NSScriptCommand
   def performDefaultImplementation
     alarms = AlarmCollection.unserialize_from_defaults.alarms
     alarms.map!.with_index { |alarm, index| "#{index}: #{alarm.to_menu_string}" }
+    return 'No alarms currently set' if alarms.empty?
     alarms.join("\n")
   end
 end
