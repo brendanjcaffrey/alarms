c302ee53ac0c640baee7650fff7eebfdaeed0473
Brendan Caffrey <brendan@jcaffrey.com>
2016-04-01 17:59:32 -0400

    update lifx to creep up to full brightness then reset to something normal when the alarm is turned off

--- app/actions/lifx_action.rb
+++ app/actions/lifx_action.rb
@@ -1,6 +1,8 @@
 class LifxAction < Action
-  @@final_brightness = 26214
-  @@kelvins = 4500
+  MAX_BRIGHTNESS    = 65535
+  MAX_KELVINS       = 6000
+  NORMAL_BRIGHTNESS = 26214
+  NORMAL_KELVINS    = 4500
 
   def prestarted(seconds_until_started)
     finished if @fd || @tick_timer
@@ -11,7 +13,7 @@ class LifxAction < Action
 
     # turn lights off, set to super low color, then turn lights on
     lifx_lan_lights_off(@fd, 254)
-    lifx_lan_set_color(@fd, 255, 0, 0, 0, @@kelvins, 0)
+    lifx_lan_set_color(@fd, 255, 0, 0, 0, MAX_KELVINS, 0)
     lifx_lan_lights_on(@fd, 0)
 
     @tick_timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self,
@@ -19,18 +21,28 @@ class LifxAction < Action
   end
 
   def finished
-    lifx_lan_close_socket(@fd) if @fd
-    @tick_timer.invalidate if @tick_timer
-    @fd = @tick_timer = nil
+    stop_ticking
+    return unless @fd
+
+    lifx_lan_set_color(@fd, 127, 0, 0, NORMAL_BRIGHTNESS, NORMAL_KELVINS, 0)
+    lifx_lan_close_socket(@fd)
+    @fd = nil
   end
 
   def tick_timer_fired(sender)
     @tick += 1
 
     fractional_tick = (@tick * 1.0) / @total_ticks
-    brightness = fractional_tick * @@final_brightness
+    brightness = fractional_tick * MAX_BRIGHTNESS
 
-    lifx_lan_set_color(@fd, @tick % 255, 0, 0, brightness, @@kelvins, 0)
-    finished if @tick >= @total_ticks
+    lifx_lan_set_color(@fd, @tick % 255, 0, 0, brightness, MAX_KELVINS, 0)
+    stop_ticking if @tick >= @total_ticks
+  end
+
+  private
+
+  def stop_ticking
+    @tick_timer.invalidate if @tick_timer
+    @tick_timer = nil
   end
 end
