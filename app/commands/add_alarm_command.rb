fbcb78688c4966b8bcf427ee6b590a9df3b0d480
Brendan Caffrey <brendan@jcaffrey.com>
2016-04-18 23:18:51 -0500

    fix applescript output showing stale data

--- app/commands/add_alarm_command.rb
+++ app/commands/add_alarm_command.rb
@@ -1,12 +1,10 @@
 class AddAlarmCommand < ListAllAlarmsCommand
+  ERROR_MSG = "Unable to add alarm, too close to another.\n\n"
+
   def performDefaultImplementation
     alarm = Alarm.from_time(self.directParameter)
-    return_string = super
+    delegate = NSApplication.sharedApplication.delegate
 
-    if !NSApplication.sharedApplication.delegate.alarm_added(alarm)
-      return_string = "Unable to add alarm, too close to another.\n\n" + return_string
-    end
-
-    return_string
+    (delegate.alarm_added(alarm) ? "" : ERROR_MSG) + super
   end
 end
--- app/commands/update_alarm_command.rb
+++ app/commands/update_alarm_command.rb
@@ -1,13 +1,11 @@
 class UpdateAlarmCommand < ListAllAlarmsCommand
+  ERROR_MSG = "Unable to update alarm, too close to another.\n\n"
+
   def performDefaultImplementation
-    return_string = super
-    new_alarm = Alarm.from_time(arguments['time'])
+    index = directParameter
+    alarm = Alarm.from_time(arguments['time'])
     delegate = NSApplication.sharedApplication.delegate
 
-    if !delegate.alarm_edited_at_index(directParameter, new_alarm)
-      return_string = "Unable to update alarm, too close to another.\n\n" + return_string
-    end
-
-    return_string
+    (delegate.alarm_edited_at_index(index, alarm) ? "" : ERROR_MSG) + super
   end
 end
