73158f2a7183511b46a0b2ccd6063ac5648fd160
Brendan Caffrey <brendan@jcaffrey.com>
2018-02-04 16:12:12 -0600

    change lifx action to turn lights all the way up when alarm is started

--- app/actions/lifx_action.rb
+++ app/actions/lifx_action.rb
@@ -1,60 +1,23 @@
 class LifxAction < Action
-  MAX_BRIGHTNESS    = 65535
-  MAX_KELVINS       = 6000
-  NORMAL_BRIGHTNESS = 26214
-  NORMAL_KELVINS    = 4500
-  LIGHT_IDS         = [39977586357200, 135995858449360, 51926218929104]
+  MAX_BRIGHTNESS = 65535
+  MAX_KELVINS    = 6000
+  LIGHT_IDS      = [130107726722000]
 
-  def prestarted(seconds_until_started)
-    finished if @fd || @tick_timer
-
+  def started
     @seqnum = 0
-    @tick = 0
-    @total_ticks = seconds_until_started.floor
-    @fd = lifx_lan_open_socket
+    fd = lifx_lan_open_socket
 
     # turn lights off, set to super low color, then turn lights on
     LIGHT_IDS.each do |id|
-      lifx_lan_lights_off(@fd, next_seqnum, id)
-      lifx_lan_set_color(@fd, next_seqnum, id, 0, 0, 0, MAX_KELVINS, 0)
-      lifx_lan_lights_on(@fd, next_seqnum, id)
+      lifx_lan_lights_off(fd, next_seqnum, id)
+      lifx_lan_set_color(fd, next_seqnum, id, 0, 0, MAX_BRIGHTNESS, MAX_KELVINS, 0)
+      lifx_lan_lights_on(fd, next_seqnum, id)
     end
 
-    @tick_timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self,
-      selector: 'tick_timer_fired:', userInfo: nil, repeats: true)
-  end
-
-  def finished
-    stop_ticking
-    return unless @fd
-
-    LIGHT_IDS.each do |id|
-      lifx_lan_set_color(@fd, next_seqnum, id, 0, 0, NORMAL_BRIGHTNESS, NORMAL_KELVINS, 0)
-    end
-
-    lifx_lan_close_socket(@fd)
-    @fd = nil
-  end
-
-  def tick_timer_fired(sender)
-    @tick += 1
-
-    fractional_tick = (@tick * 1.0) / @total_ticks
-    brightness = fractional_tick * MAX_BRIGHTNESS
-
-    LIGHT_IDS.each do |id|
-      lifx_lan_set_color(@fd, next_seqnum, id, 0, 0, brightness, MAX_KELVINS, 0)
-    end
-
-    stop_ticking if @tick >= @total_ticks
+    lifx_lan_close_socket(fd)
   end
 
   private
-
-  def stop_ticking
-    @tick_timer.invalidate if @tick_timer
-    @tick_timer = nil
-  end
 
   def next_seqnum
     @seqnum += 1
